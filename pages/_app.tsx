1671d06181d40ddc6b3d2e8a36e3f889951a4afc
Temitope3665 <aroyewontope@gmail.com>
2023-08-20 19:23:25 +0100

    clean

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
-import '../styles/globals.css';
+/* eslint-disable max-len */
+/* eslint-disable react/jsx-no-bind */
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import NProgress from 'nprogress';
+import Interceptor from '../services/interceptor';
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  NProgress.configure({
+    easing: 'ease',
+    speed: 500,
+  });
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+  return (
+    <Interceptor
+      component={(
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      )}
+    />
+  );
+};
 
 export default MyApp;
